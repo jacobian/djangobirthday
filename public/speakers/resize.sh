94de4ea44038b51428866c1e73760efed120b548
Jacob Kaplan-Moss <jacob@jacobian.org>
2015-07-01 14:47:20 -0700

    Fix resize.sh to actually resize.
    Also fix the filename extra-suffix thing.

--- public/speakers/resize.sh
+++ public/speakers/resize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-convert $1 -size 640x640 $1.640w.jpg
-convert $1 -size 510x510 $1.510w.jpg
-convert $1 -size 255x255 $1.255w.jpg
+bn=`basename -s .jpg $1`
+convert $1 -resize 640x640 $bn.640w.jpg
+convert $1 -resize 510x510 $bn.510w.jpg
+convert $1 -resize 255x255 $bn.255w.jpg
